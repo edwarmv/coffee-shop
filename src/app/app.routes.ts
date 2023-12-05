c784876f96855e4d683eefbf32a7116e011e0b8b
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-12-04 22:26:20 -0400

    feat: add shopping-cart, favories, and notifications pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,25 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/home/home.component').then((c) => c.HomeComponent),
   },
+  {
+    path: 'shopping-cart',
+    loadComponent: () =>
+      import('./pages/shopping-cart/shopping-cart.component').then(
+        (c) => c.ShoppingCartComponent,
+      ),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./pages/favorites/favorites.component').then(
+        (c) => c.FavoritesComponent,
+      ),
+  },
+  {
+    path: 'notifications',
+    loadComponent: () =>
+      import('./pages/notifications/notifications.component').then(
+        (c) => c.NotificationsComponent,
+      ),
+  },
 ];
